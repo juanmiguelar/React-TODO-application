eda7d1708cc7906bc22c95b9d068f43763ea84b2
Juan Miguel <juanmiguelar09@gmail.com>
2021-03-10 02:58:19 +0000

    Inicio de listas

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,42 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
 
 //create your first component
 export function Home() {
+	const [task, setTask] = useState("");
+
+	const handleOnKeyDown = e => {
+		if (e.key === "Enter") {
+			//code to execute here
+			console.log("La tarea es " + task);
+		}
+	};
+
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<h1 className="text-muted text-center">todos</h1>
+			<input
+				type="text"
+				className="form-control mb-2 text-secondary"
+				value={task}
+				placeholder="Type your task"
+				onChange={e => setTask(e.target.value)}
+				onKeyDown={e => handleOnKeyDown(e)}
+			/>
+			<ul className="list-group">
+				<li className="list-group-item text-secondary">
+					<p className="d-inline-block ml-4">Make the bed</p>
+					<button className="btn btn-outline-secondary float-right">
+						<i className="fas fa-times"></i>
+					</button>
+				</li>
+			</ul>
+			<div className="card">
+				<ul className="list-group list-group-flush">
+					<li className="list-group-item text-secondary ml-4">
+						4 items left
+					</li>
+				</ul>
+			</div>
 		</div>
 	);
 }
